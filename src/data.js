27d2468464b6d0a23256e600a77caaee016b9274
Osama-Aamer <osamaaamer2@gmail.com>
2025-09-10 14:41:29 +0300

    interation 1 fix git

--- src/data.js
+++ src/data.js
@@ -3,11 +3,12 @@ import tour2 from "./assets/images/tour-2.jpeg";
 import tour3 from "./assets/images/tour-3.jpeg";
 import tour4 from "./assets/images/tour-4.jpeg";
 
+
 export const pageLinks = [
-  { id: 1, href: "#home", text: "home" },
-  { id: 2, href: "#about", text: "about" },
-  { id: 3, href: "#services", text: "services" },
-  { id: 4, href: "#tours", text: "tours" },
+  { id: 1, href: "/", text: "home" },
+  { id: 2, href: "/about", text: "about" },
+  { id: 3, href: "/services", text: "services" },
+  { id: 4, href: "/tours", text: "tours" },
 ];
 
 export const socialLinks = [
