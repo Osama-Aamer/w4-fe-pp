c544c081a7d02ab00d5d587f691f045f3482cffc
Tanvir Rahman Nibir <TANVIR.NIBIR@metropolia.fi>
2025-09-10 17:37:10 +0300

    Succesfully completed Pair-Programming

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -5,17 +5,28 @@ import Tour from "./Tour";
 
 const Tours = () => {
   const [toursData, setToursData] = useState(tours);
+  const removeTour = (id) => {
+    setToursData(toursData.filter((tour) => tour.id !== id));
+  };
+  const restoreTours = () => {
+    setToursData(tours);
+  };
 
   return (
     <section className="section" id="tours">
+
+
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
         {toursData.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+          return <Tour key={tour.id} {...tour}
+            removeTour = {() => removeTour(tour.id)} />;
         })}
       </div>
-    </section>
+      <button onClick={restoreTours}>Restore</button>
+
+    </section>  
   );
 };
 export default Tours;
